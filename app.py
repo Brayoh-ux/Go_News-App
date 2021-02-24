00489296c428e8f9801258ed98f37f0139ad988f
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-02-24 10:41:28 +0300

    add app flask

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 from newsapi import NewsApiClient
 from flask_bootstrap import Bootstrap
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 # bootstrap = Bootstrap(app)
 # @app.route('/')

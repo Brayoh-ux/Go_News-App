737faa403d6dbe04e29c370d71f099e64ba316c0
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-02-25 08:18:36 +0300

    deploy 2 heroku

---
+++ README.md
@@ -0,0 +1,2 @@
+## GO NEWS APP
+This app enables you to browse and see latest news all over the world 
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 # from app import views
 from views import app
-from flask import Flask
 
-app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run()
